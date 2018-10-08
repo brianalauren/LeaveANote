b14e49547897b43b013a196d95a18ee0c48cfc60
Briana Gude <briana.gude@gmail.com>
2018-10-08 19:22:58 -0400

    initial setup

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,83 @@
 class NotesController < ApplicationController
+
+  def index
+    @note = Note.new
+  end
+
+
+
+  def all
+    @notes = Note.all
+  end
+
+
+  def create
+    @note = Note.new(form_params)
+
+
+    if @note.save
+      redirect_to root_path
+    else
+      render "index"
+    end
+  end
+
+
+
+
+  def edit
+    @note = Note.find(params[:id])
+
+    if @note.created_at < 5.minutes.ago
+      redirect_to note_path(@note)
+    end
+  end
+
+
+
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+
+
+
+  def update
+    @note = Note.find(params[:id])
+
+
+    if @note.created_at < 5.minutes.ago
+      redirect_to root_path
+    else
+      if @note.update(form_params)
+
+        redirect_to note_path(@note)
+      else
+        render "edit"
+      end
+    end
+  end
+
+
+
+
+  def destroy
+    @note = Note.find(params[:id])
+
+    if @note.created_at < 5.minutes.ago
+      @note.destroy
+    end
+
+    redirect_to root_path
+  end
+
+
+
+
+
+  def form_params
+    params.require(:note).permit(:body)
+  end
+
 end
