8a9d49c0ec6d1c63153845ed4a623360f4943af7
Briana Gude <briana.gude@gmail.com>
2018-10-16 18:15:20 -0400

    fixed session error

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -12,21 +12,23 @@ class NotesController < ApplicationController
 
   def random
     @note = Note.order('RANDOM()').first
-    
   end
 
 
   def create
     @note = Note.new(form_params)
+    session[:current_user_id] = @user.id
 
     if @note.save
       flash[:success] = "Your note has been submitted!"
-      if session[:notes].present?
-        session[:notes].push(@note.id)
-      else
-        session[:notes] = [@note.id]
-      end
-      redirect_to root_path
+      session[:current_user_id].push(@note.id)
+
+      # if session[:notes].present?
+      #   session[:notes].push(@note.id)
+      # else
+      #   session[:notes] = [@note.id]
+      # end
+      # redirect_to root_path
     else
       render "index"
     end
@@ -37,9 +39,9 @@ class NotesController < ApplicationController
   def edit
     @note = Note.find(params[:id])
 
-    if @note.created_at < 5.minutes.ago
-      redirect_to note_path(@note)
-    end
+    # if @note.created_at < 5.minutes.ago
+    #   redirect_to note_path(@note)
+    # end
     flash[:success] = 'Your note has been submitted!'
   end
 
@@ -53,27 +55,24 @@ class NotesController < ApplicationController
     @note = Note.find(params[:id])
 
 
-    if @note.created_at < 5.minutes.ago
-      redirect_to root_path
-    else
+    # if @note.created_at < 5.minutes.ago
+    #   redirect_to root_path
+    # else
       if @note.update(form_params)
-
         redirect_to note_path(@note)
       else
         render "edit"
       end
-    end
+    # end
   end
 
 
   def destroy
     @note = Note.find(params[:id])
-
     @note.destroy
 
     redirect_to random_path
     flash[:success] = 'Your note has been deleted!'
-
   end
 
 
