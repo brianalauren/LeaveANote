8785f4d9e3e093a8f6ae69a7decbcec4fe9dce78
Briana Gude <briana.gude@gmail.com>
2018-10-21 14:17:55 -0400

    minor

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,9 +1,9 @@
 class Note < ApplicationRecord
 
   has_many :notes
-  profanity_filter :body, :method => 'vowels'
+  # profanity_filter :body, :method => 'vowels'
 
-  validates :body, presence: true, length: {minimum: 3, maximum: 250}, uniqueness: true
+  validates :body, presence: true, length: {minimum: 1, maximum: 250}, uniqueness: true
 
   def to_param
     id.to_s + "-" + body.parameterize
