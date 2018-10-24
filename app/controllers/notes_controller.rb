24995308d7735325c9906cccf00c0d406111da09
Briana Gude <briana.gude@gmail.com>
2018-10-24 03:18:24 -0400

    fixed edit issue

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -18,53 +18,43 @@ class NotesController < ApplicationController
 
   def create
     @note = Note.new(form_params)
-      if session[:notes].present? and @note.save
+
+    if @note.save
+      flash[:success] = "Your note has been submitted!"
+      if session[:notes].present?
         session[:notes].push(@note.id)
-        redirect_to root_path
-        flash[:success] = 'Your note has been submitted!'
       else
         session[:notes] = [@note.id]
-        render "index"
-        # flash[:error] = 'Your note must be between 3 and 150 characters!'
       end
+      redirect_to root_path
+    else
+      render "index"
+    end
 
   end
 
 
   def edit
     @note = Note.find(params[:id])
-
-    # if @note.created_at < 5.minutes.ago
-    #   redirect_to note_path(@note)
-    # end
-    flash[:success] = 'Your note has been submitted!'
   end
 
 
   def show
     @note = Note.find(params[:id])
-    if session[:notes].present?
-      session[:notes].push(@note.id)
-    else
-      session[:notes] = [@note.id]
-    end
-
   end
 
 
   def update
     @note = Note.find(params[:id])
 
-
-    # if @note.created_at < 5.minutes.ago
-    #   redirect_to root_path
-    # else
       if @note.update(form_params)
         redirect_to note_path(@note)
+        flash[:success] = 'Your note has been updated!'
+
       else
         render "edit"
       end
-    # end
+
   end
 
 
